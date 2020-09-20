c4ef0549c4b6c7c69ff9e70c9be2e6f95dc9fe50
Jed Fox <git@jedfox.com>
2020-09-20 16:18:58 -0400

    Updates

--- index.js
+++ index.js
@@ -9,30 +9,46 @@ const readStream = process.argv[2]
   ? fs.createReadStream(process.argv[2], { encoding: "utf8" })
   : process.stdin;
 
+function handle(chunk, push) {
+  const combined = state.buf + chunk;
+  const lastNewline = combined.lastIndexOf("\n");
+  const lines = combined.slice(0, lastNewline).split("\n");
+  let line = lines.shift();
+  while (line != null) {
+    if (state.inside) {
+      if (line === "</pre>") {
+        state.inside = false;
+      } else if (line.includes("</pre>")) {
+        state.inside = false;
+        push(line.slice(0, line.indexOf("</pre>")) + "\n");
+      } else {
+        push(line + "\n");
+      }
+    } else {
+      if (startRe.test(line)) {
+        state.inside = true;
+      }
+    }
+    line = lines.shift();
+  }
+  state.buf = combined.slice(lastNewline + 1);
+}
+
 readStream
   .pipe(
     new Transform({
       transform(/** @type {Buffer} */ chunk, _, callback) {
-        const combined = state.buf + chunk.toString("utf8");
-        const lastNewline = combined.lastIndexOf("\n");
-        const lines = combined.slice(0, lastNewline).split("\n");
-        let line;
-        do {
-          line = lines.shift();
-          if (state.inside) {
-            if (line === "</pre>") {
-              state.inside = false;
-            } else {
-              this.push(line + "\n");
-            }
-          } else {
-            if (startRe.test(line)) {
-              state.inside = true;
-            }
-          }
-        } while (line != null);
-        state.buf = lines.join("\n") + combined.slice(lastNewline);
+        const push = (content) => this.push(content.replace(/&lt;/g, "<"));
+        handle(chunk.toString("utf8"), push);
         callback();
+      },
+      final(callback) {
+        handle("\n");
+        if (!["", "\n"].includes(state.buf) || state.inside) {
+          callback(new Error("Invalid file: " + JSON.stringify(state)));
+        } else {
+          callback();
+        }
       },
     })
   )
